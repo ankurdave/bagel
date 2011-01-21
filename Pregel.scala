02ac5df9c490f9c32c04f4f2da171c86abd04447
Ankur Dave <ankurdave@gmail.com>
2011-01-20 22:38:00 -0800

    Moved RDDExtensions to separate file.

--- Pregel.scala
+++ Pregel.scala
@@ -3,17 +3,14 @@ import spark.SparkContext._
 
 import scala.collection.mutable.ArrayBuffer
 
-
 object Pregel {
-  implicit def RDDExtensions[T](self: RDD[T]) = new RDDExtensions(self)
-  implicit def PairRDDExtensions[K,V](self: RDD[(K, V)]) = new PairRDDExtensions(self)
-
   def run[V,M,E](vertices: RDD[Vertex[V,E]], messages: RDD[Message[M]])(compute: (Vertex[V,E], Iterable[Message[M]]) => (Vertex[V,E], Iterable[Message[M]])): Iterable[Vertex[V,E]] = {
-    println("Vertices:")
-    println(vertices.map(_.toString).collect.mkString("\n"))
-    println("Messages:")
-    println(messages.map(_.toString).collect.mkString("\n"))
-    Console.readLine("Press Enter... ")
+    println("Vertices:\n" + 
+            vertices.map("\t" + _.toString).collect.mkString("\n") + "\n" +
+            "Messages:\n" +
+            messages.map("\n" + _.toString).collect.mkString("\n") + "\n")
+    
+    // Console.readLine("Press Enter... ")
 
     val verticesWithId = vertices.map(v => (v.id, v))
     val messagesWithId = messages.map(m => (m.targetId, m))
@@ -43,6 +40,9 @@ object Pregel {
     else
       run(newVertices, newM)(compute)
   }
+
+  implicit def RDDExtensions[T](self: RDD[T]) = new RDDExtensions(self)
+  implicit def PairRDDExtensions[K,V](self: RDD[(K, V)]) = new PairRDDExtensions(self)
 }
 
 case class Message[A](targetId: String, value: A)
@@ -55,41 +55,3 @@ case class Edge[A](targetId: String, value: A) {
 sealed abstract class VertexState
 case object Active extends VertexState
 case object Inactive extends VertexState
-
-class RDDExtensions[T](self: RDD[T]) {
-  def exists(p: T => Boolean): Boolean = {
-    self.map(p).reduce(_ || _)
-  }
-
-  def forall(p: T => Boolean): Boolean = {
-    self.map(p).reduce(_ && _)
-  }
-}
-
-class PairRDDExtensions[K, V](self: RDD[(K, V)]) {
-  // Differs from the standard join in two ways: (1) a pair from one
-  // RDD is included even if there's no corresponding pair in the
-  // other RDD, and (2) multiple values are grouped together in a Seq
-  // rather than forming the Cartesian product.
-  def outerJoin[W](other: RDD[(K, W)], numSplits: Int): RDD[(K, (Seq[V], Seq[W]))] = {
-    val vs: RDD[(K, Either[V, W])] = self.map { case (k, v) => (k, Left(v)) }
-    val ws: RDD[(K, Either[V, W])] = other.map { case (k, w) => (k, Right(w)) }
-    (vs ++ ws).groupByKey(numSplits).map {
-      case (k, seq) => {
-        val vbuf = new ArrayBuffer[V]
-        val wbuf = new ArrayBuffer[W]
-        seq.foreach(_ match {
-          case Left(v) => vbuf += v
-          case Right(w) => wbuf += w
-        })
-        (k, (vbuf.toList, wbuf.toList))
-      }
-    }
-  }
-
-  def outerJoin[W](other: RDD[(K, W)]): RDD[(K, (Seq[V], Seq[W]))] = {
-    outerJoin(other, numCores)
-  }
-
-  def numCores = self.sparkContext.numCores
-}
---
+++ RDDExtensions.scala
@@ -0,0 +1,40 @@
+import spark._
+import spark.SparkContext._
+
+class RDDExtensions[T](self: RDD[T]) {
+  def exists(p: T => Boolean): Boolean = {
+    self.map(p).reduce(_ || _)
+  }
+
+  def forall(p: T => Boolean): Boolean = {
+    self.map(p).reduce(_ && _)
+  }
+}
+
+class PairRDDExtensions[K, V](self: RDD[(K, V)]) {
+  // Differs from the standard join in two ways: (1) a pair from one
+  // RDD is included even if there's no corresponding pair in the
+  // other RDD, and (2) multiple values are grouped together in a Seq
+  // rather than forming the Cartesian product.
+  def outerJoin[W](other: RDD[(K, W)], numSplits: Int): RDD[(K, (Seq[V], Seq[W]))] = {
+    val vs: RDD[(K, Either[V, W])] = self.map { case (k, v) => (k, Left(v)) }
+    val ws: RDD[(K, Either[V, W])] = other.map { case (k, w) => (k, Right(w)) }
+    (vs ++ ws).groupByKey(numSplits).map {
+      case (k, seq) => {
+        val vbuf = new ArrayBuffer[V]
+        val wbuf = new ArrayBuffer[W]
+        seq.foreach(_ match {
+          case Left(v) => vbuf += v
+          case Right(w) => wbuf += w
+        })
+        (k, (vbuf.toList, wbuf.toList))
+      }
+    }
+  }
+
+  def outerJoin[W](other: RDD[(K, W)]): RDD[(K, (Seq[V], Seq[W]))] = {
+    outerJoin(other, numCores)
+  }
+
+  def numCores = self.sparkContext.numCores
+}
